dee566af248b8064e9150d5ef59475364f668b03
sashacrossley <sasha.m.crossley@gmail.com>
2020-04-25 13:44:03 +0100

    Phase1

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,30 @@
 class PolyTreeNode
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent #returns the node's parent
+        @parent
+    end
+
+    def children #returns an array of children of a node
+        @children
+    end
+
+    def value #returns the value stored at the node
+        @value
+    end
+
+    def parent=(pnode) #(1) sets the parent property and (2) adds the node to their parent's array of children (unless we're setting parent to nil).
+        @parent = pnode
+        unless @parent.nil? 
+            unless pnode.children.include?(self)
+                pnode.children << self
+            end
+        end
+    end
 
 end
