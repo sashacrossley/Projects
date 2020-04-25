90e82a67cc5a7544ffc2864c079c6554bacf098b
sashacrossley <sasha.m.crossley@gmail.com>
2020-04-25 14:14:29 +0100

    Phase 2

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -18,7 +18,10 @@ class PolyTreeNode
         @value
     end
 
-    def parent=(pnode) #(1) sets the parent property and (2) adds the node to their parent's array of children (unless we're setting parent to nil).
+    def parent=(pnode) #(1) sets the parent property and (2) adds the node to their parent's array of children (unless we're setting parent to nil). we should also remove the child from the old parent's list of children (if the old parent isn't nil). Modify your #parent= method to do this.
+        unless @parent.nil?
+            @parent.children.delete_if {|child| child == self}
+        end
         @parent = pnode
         unless @parent.nil? 
             unless pnode.children.include?(self)
