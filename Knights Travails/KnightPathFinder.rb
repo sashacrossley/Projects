932fd64d644c4017df155e801aa561cc72870d99
sashacrossley <sasha.m.crossley@gmail.com>
2020-04-26 10:53:28 +0100

    KT Part1 Complete

--- Knights Travails/KnightPathFinder.rb
+++ Knights Travails/KnightPathFinder.rb
@@ -13,11 +13,12 @@ class KnightPathFinder
         [1, 2]
     ].freeze
 
-    attr_reader :starting_pos
+    attr_reader :starting_pos, :considered_positions
+    attr_accessor :root_node
 
     def initialize(starting_pos)
         @starting_pos = starting_pos
-        build_move_tree
+        # build_move_tree
         @considered_positions = [starting_pos] #keep track of the positions you have considered
     end
 
@@ -36,13 +37,19 @@ class KnightPathFinder
 
         valid_coord
     end
-    
-    def self.root_node
-        @root_node = PolyTreeNode.new(starting_pos)
-    end
 
     def build_move_tree 
-
+        self.root_node = PolyTreeNode.new(starting_pos)
+        queue = [root_node]
+        until queue.empty?
+            current_node = queue.shift
+            position = current_node.value
+            new_move_positions(position).each do |child|
+                child_node = PolyTreeNode.new(child)
+                current_node.add_child(child_node)
+                queue << child_node
+            end
+        end
     end
 
     def find_path
@@ -50,7 +57,19 @@ class KnightPathFinder
     end
 
     def new_move_positions(pos) #call the ::valid_moves class method, but filter out any positions that are already in @considered_positions. It should then add the remaining new positions to @considered_positions and return these new positions.
-
+        new_moves = []
+        KnightPathFinder.valid_moves(pos).each do |pos|
+            unless considered_positions.include?(pos)
+                new_moves << pos
+                considered_positions << pos
+            end
+        end
+        new_moves
     end
 
 end
+
+a = KnightPathFinder.new([0,0])
+# p KnightPathFinder.valid_moves([2,1])
+# p a.new_move_positions([2,1])
+p a.build_move_tree
