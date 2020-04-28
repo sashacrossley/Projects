3aa3cd0b75e4bfa08f8c0fd0d4b0ca9831cda7bb
sashacrossley <sasha.m.crossley@gmail.com>
2020-04-28 18:16:37 +0100

    Phase 2 Complete

--- TicTacToe AI/lib/super_computer_player.rb
+++ TicTacToe AI/lib/super_computer_player.rb
@@ -2,6 +2,29 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    winning_node = nil
+    
+    #If any of the children is a winning_node? for the mark passed in to the #move method, return that node's prev_move_pos
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        winning_node = child
+        break
+      end
+    end
+
+    return winning_node.prev_move_pos if winning_node
+
+    #If none of the children of the node we created are winning_node?s, that's ok. We can just pick one that isn't a losing_node? and return its prev_move_pos
+    node.children.each do |child|
+      if !child.losing_node?(mark)
+        winning_node = child
+        break
+      end
+    end
+
+    return winning_node.prev_move_pos if winning_node
+    raise "Tis isn't fair"
   end
 end
 
