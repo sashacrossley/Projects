b091e8a00cdf8cf8b342cff1150a3520da6e654f
sashacrossley <sasha.m.crossley@gmail.com>
2020-06-21 12:09:40 +0100

    phase 6 complete

--- Chess/display.rb
+++ Chess/display.rb
@@ -1,37 +1,39 @@
 require 'colorize'
 require_relative './cursor.rb'
-require_relative './board.rb'
 
 class Display
     attr_reader :cursor
     def initialize(board)
         @board = board
         @cursor = Cursor.new([0,0], board)
+        @notifications = {}
     end
 
     def render
-        @board.rows.map.with_index do |row, i|
-            row.map.with_index do |tile, j|
-                if cursor.cursor_pos == [i,j] && cursor.selected
-                    tile.symbol.colorize(tile.colour).colorize(:background =>:green)
-                elsif cursor.cursor_pos == [i,j]
-                    tile.symbol.colorize(tile.colour).colorize(:background =>:red)
-                elsif (i+j).odd?
-                    tile.symbol.colorize(tile.colour).colorize(:background =>:light_blue)
-                else
-                    tile.symbol.colorize(tile.colour).colorize(:background =>:light_black)
-                end
-            end.join("")
-        end.join("\n")
+        system("clear")
+        puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
+            @board.rows.map.with_index do |row, i|
+                row.map.with_index do |tile, j|
+                    if cursor.cursor_pos == [i,j] && cursor.selected
+                        tile.symbol.colorize(tile.colour).colorize(:background =>:green)
+                    elsif cursor.cursor_pos == [i,j]
+                        tile.symbol.colorize(tile.colour).colorize(:background =>:red)
+                    elsif (i+j).odd?
+                        tile.symbol.colorize(tile.colour).colorize(:background =>:light_blue)
+                    else
+                        tile.symbol.colorize(tile.colour).colorize(:background =>:light_black)
+                    end
+                end.join("")
+            end.join("\n")
     end
     
-    def test
-        loop do
-            system("clear")
-            puts render
-            cursor.get_input
-        end
-    end
+    # def test
+    #     loop do
+    #         system("clear")
+    #         puts render
+    #         cursor.get_input
+    #     end
+    # end
 
 end
 
---
+++ Chess/player.rb
@@ -0,0 +1,8 @@
+class Player
+    attr_reader :colour, :display
+    
+    def initialize(colour, display)
+        @colour = colour
+        @display = display
+    end
+end
