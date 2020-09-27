59e052d6a8462f926fde30bd00c66352a46e50cb
sashacrossley <sasha.m.crossley@gmail.com>
2020-09-27 17:35:53 +0100

    phase 4 done

--- SQL Zoo/lib/04_select_within_select.rb
+++ SQL Zoo/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      population > (
+        SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Russia'
+      );
   SQL
 end
 
@@ -42,6 +55,20 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      countries.continent = 'Europe'
+      AND (countries.gdp/countries.population) > (
+        SELECT
+          c2.gdp / c2.population
+        FROM
+          countries c2
+        WHERE
+          c2.name = 'United Kingdom'
+      );
   SQL
 end
 
@@ -49,6 +76,20 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT
+      name,
+      continent
+    FROM
+      countries
+    WHERE
+      continent IN (
+      SELECT
+        c2.continent
+      FROM
+        countries c2
+      WHERE
+        c2.name = 'Belize' OR c2.name ='Belgium'
+    );
   SQL
 end
 
@@ -56,6 +97,29 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT
+      countries.name,
+      countries.population
+    FROM
+      countries
+    WHERE
+      countries.population > (
+        SELECT
+          c2.population
+        FROM
+          countries c2
+        WHERE
+          c2.name = 'Canada'
+      )
+      AND
+      countries.population < (
+        SELECT
+          c2.population
+        FROM
+          countries c2
+        WHERE
+          c2.name = 'Poland'
+      );
   SQL
 end
 
@@ -65,5 +129,20 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+    SELECT
+      countries.name,
+      countries.continent,
+      countries.population
+    FROM
+      countries
+    WHERE
+      countries.continent NOT IN (
+        SELECT
+          c2.continent
+        FROM
+          countries c2
+        WHERE
+          c2.population >=25000000
+      );
   SQL
 end
