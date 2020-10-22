03abdaf62e41438169f9797fbca20bfd2099aeaa
sashacrossley <sasha.m.crossley@gmail.com>
2020-10-22 21:35:00 +0100

    database seeded

--- AA Questions/import_db.sql
+++ AA Questions/import_db.sql
@@ -12,6 +12,12 @@ CREATE TABLE users (
     lname TEXT NOT NULL
 );
 
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ("AA", "BB"), ("CC","DD");
+
+
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
@@ -20,6 +26,12 @@ CREATE TABLE questions (
     
     FOREIGN KEY(author_id) REFERENCES users(id)
 );
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ("Q1", "body of Q1", (SELECT id FROM users WHERE fname = "AA" AND lname = "BB")),
+    ("Q2", "body of Q2", (SELECT id FROM users WHERE fname = "CC" AND lname = "DD"));
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
@@ -30,18 +42,47 @@ CREATE TABLE question_follows (
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
+INSERT INTO
+    question_follows(author_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE fname = "AA" AND lname = "BB"),
+    (SELECT id FROM questions where title = "Q1")),
+
+    ((SELECT id FROM users WHERE fname = "CC" AND lname = "DD"),
+    (SELECT id FROM questions where title = "Q1"));
+
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     author_id INTEGER NOT NULL,
     body TEXT NOT NULL,
-    parent_reply_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
 
 
     FOREIGN KEY(question_id) REFERENCES questions(id),
     FOREIGN KEY(author_id) REFERENCES users(id),
     FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
 );
+
+INSERT INTO
+    replies (question_id, author_id, body, parent_reply_id)
+VALUES
+    (
+        (SELECT id FROM questions WHERE title = "Q1"),
+        (SELECT id FROM users WHERE fname = "AA" AND lname = "BB"),
+        "parent Reply",
+        NULL
+    );
+
+INSERT INTO
+    replies (question_id, author_id, body, parent_reply_id)
+VALUES   
+    (
+        (SELECT id FROM questions WHERE title = "Q1"),
+        (SELECT id FROM users WHERE fname = "AA" AND lname = "BB"),
+        "Child Reply",
+        (SELECT id FROM replies WHERE body = "parent Reply")
+    );
 
 CREATE TABLE question_likes(
     id INTEGER PRIMARY KEY,
@@ -51,3 +92,6 @@ CREATE TABLE question_likes(
     FOREIGN KEY(author_id) REFERENCES users(id),
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
+
+INSERT INTO question_likes (author_id, question_id) VALUES (1, 1);
+INSERT INTO question_likes (author_id, question_id) VALUES (1, 2);
