7275dd049c97d8e20d3e4aad842dd0216834213b
sashacrossley <sasha.m.crossley@gmail.com>
2021-03-21 17:58:38 +0000

    polls complete

--- Polls/polls_app/app/models/question.rb
+++ Polls/polls_app/app/models/question.rb
@@ -19,4 +19,49 @@ class Question < ApplicationRecord
     through: :answer_choices,
     source: :responses
 
+    def results
+        # n+1 
+        # answer_hash = {}
+        # all_choices = self.answer_choices.all
+        # all_choices.each do |choice|
+        #     answer_hash[choice.text] = choice.responses.length
+        # end
+        # answer_hash
+
+        # using includes
+        # answer_hash = {}
+        # self.answer_choices.includes(:responses).each do |choice|
+        #     answer_hash[choice.text] = choice.responses.length
+        # end
+        # answer_hash
+
+        # 1 query - using SQL
+        # acs = AnswerChoice.find_by_sql([<<-SQL, id])
+        #     SELECT
+        #         answer_choices.text, COUNT(responses.id) AS num_responses
+        #     FROM
+        #         answer_choices
+        #         LEFT OUTER JOIN responses
+        #         ON answer_choices.id = responses.answer_choice_id
+        #     WHERE
+        #         answer_choices.question_id = ?
+        #     GROUP BY
+        #         answer_choices.id
+        # SQL
+
+        # 1 query - using activerecord
+
+        acs = self.answer_choices
+        .left_outer_joins(:responses)
+        .select("answer_choices.text, COUNT(responses.id) AS num_responses")
+        .group("answer_choices.id")
+
+
+        answer_hash = {}
+        acs.each do |choice|
+            answer_hash[choice.text] = choice.num_responses
+        end
+        answer_hash
+    end
+
 end
