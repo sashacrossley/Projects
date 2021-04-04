e2561cf1cd829c7ec3c3bca013fcc89fff5c2e68
sashacrossley <sasha.m.crossley@gmail.com>
2021-04-04 19:37:02 +0100

    phase 1 complete

--- BYOActiveRecord/lib/01_sql_object.rb
+++ BYOActiveRecord/lib/01_sql_object.rb
@@ -89,17 +89,44 @@ class SQLObject
 
   def attribute_values
     # ...
+    self.class.columns.map { |column| self.send(column)}
   end
 
   def insert
     # ...
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_s).join(", ")
+    question_marks = (["?"] * columns.count).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
     # ...
+    col_names = self.class.columns.map {|attr| "#{attr} = ?"}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{col_names}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def save
     # ...
+    if id.nil?
+      insert
+    else
+      update
+    end
   end
 end
