ab14843453eca17698e1422f7e9c4397cdf022f0
sashacrossley <sasha.m.crossley@gmail.com>
2021-04-05 18:35:31 +0100

    phase 4 complete

--- BYOActiveRecord/lib/03_associatable.rb
+++ BYOActiveRecord/lib/03_associatable.rb
@@ -72,9 +72,10 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     # ...
-    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
     define_method(name) do
-      
+      options = self.class.assoc_options[name]
       key_value = self.send(options.foreign_key)
 
       options
@@ -100,6 +101,8 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
