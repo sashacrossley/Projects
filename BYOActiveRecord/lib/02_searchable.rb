d70724a0da0a321d7e4823d2685b6093d49c9fc9
sashacrossley <sasha.m.crossley@gmail.com>
2021-04-05 14:22:09 +0100

    phase 2 complete

--- BYOActiveRecord/lib/02_searchable.rb
+++ BYOActiveRecord/lib/02_searchable.rb
@@ -4,9 +4,27 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+    params_array = []
+    params.each do |k, v|
+      params_array << "#{k} = ?"
+    end
+
+    where_line = params_array.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
