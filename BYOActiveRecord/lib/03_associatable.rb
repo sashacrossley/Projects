448fdf323f687fc77689e6cfb45ae4db62b04862
sashacrossley <sasha.m.crossley@gmail.com>
2021-04-05 14:53:57 +0100

    phase 3a

--- BYOActiveRecord/lib/03_associatable.rb
+++ BYOActiveRecord/lib/03_associatable.rb
@@ -11,22 +11,42 @@ class AssocOptions
 
   def model_class
     # ...
+    self.class_name.constantize
   end
 
   def table_name
     # ...
+    self.class_name.downcase + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    if options.empty?
+      @foreign_key = (name.to_s.singularize.underscore + "_id").to_sym
+      @primary_key = :id
+      @class_name = name.to_s.singularize.camelcase
+    else
+      @foreign_key = options[:foreign_key]
+      @class_name = options[:class_name]
+      @primary_key = options[:primary_key]
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    if options.empty?
+      @foreign_key = (self_class_name.to_s.singularize.underscore + "_id").to_sym
+      @primary_key = :id
+      @class_name = name.to_s.singularize.camelcase
+    else
+      @foreign_key = options[:foreign_key]
+      @class_name = options[:class_name]
+      @primary_key = options[:primary_key]
+    end
   end
 end
 
@@ -46,5 +66,6 @@ module Associatable
 end
 
 class SQLObject
+  extend Associatable
   # Mixin Associatable here...
 end
